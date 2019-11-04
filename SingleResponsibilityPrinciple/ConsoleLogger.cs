26089783789a6aead197301eef2d20b1cf7330e2
smyla <smyla@EC2AMAZ-FV81E98>
2019-11-03 20:03:19 -0600

    Request  405 - Updated ConsoleLogger to write to xml log file.

--- SingleResponsibilityPrinciple/ConsoleLogger.cs
+++ SingleResponsibilityPrinciple/ConsoleLogger.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.IO;
 using SingleResponsibilityPrinciple.Contracts;
 
 namespace SingleResponsibilityPrinciple
@@ -8,12 +8,28 @@ namespace SingleResponsibilityPrinciple
     {
         public void LogWarning(string message, params object[] args)
         {
+            // Displays Message in console
             Console.WriteLine(string.Concat("WARN: ", message), args);
+
+            // writes Message in log file
+            using (StreamWriter logfile = File.AppendText("log.xml"))
+            {
+                logfile.WriteLine("<log> + <message>" + message + "</message></log> ", args);
+            }
+
         }
 
         public void LogInfo(string message, params object[] args)
         {
+            // Displays Message in console
             Console.WriteLine(string.Concat("INFO: ", message), args);
+
+            // writes Message in log file
+            using (StreamWriter logfile = File.AppendText("log.xml"))
+            {
+                logfile.WriteLine("<log> + <message>" + message + "</message></log> ", args);
+            }
         }
+
     }
 }
