ed2ba64b096f02890788f631259c18c899478f54
smyla <smyla@EC2AMAZ-FV81E98>
2019-11-03 20:57:48 -0600

    Request  407 - Created  URLTradeDataProvider object and called in main program instead of StreamTradeDataProvider and pass the new data provider to the TradeProcessor.

--- SingleResponsibilityPrinciple/Program.cs
+++ SingleResponsibilityPrinciple/Program.cs
@@ -9,17 +9,27 @@ namespace SingleResponsibilityPrinciple
     {
         static void Main(string[] args)
         {
+
+            //  Updated URLTradeDataProvider object instead of a  StreamTradeDataProvider and pass the new data provider to the TradeProcessor
             var tradeStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("SingleResponsibilityPrinciple.trades.txt");
+            string urlData = "http://faculty.css.edu/tgibbons/trades4.txt";
 
             var logger = new ConsoleLogger();
             var tradeValidator = new SimpleTradeValidator(logger);
-            var tradeDataProvider = new StreamTradeDataProvider(tradeStream);
+            var tradeURLProvider = new URLTradeDataProvider(urlData);
             var tradeMapper = new SimpleTradeMapper();
             var tradeParser = new SimpleTradeParser(tradeValidator, tradeMapper);
-            var tradeStorage = new AdoNetTradeStorage(logger);
+            var tradeStorage = new AdoNetTradeStorage(logger);        
 
-            var tradeProcessor = new TradeProcessor(tradeDataProvider, tradeParser, tradeStorage);
+            var tradeProcessor = new TradeProcessor(tradeURLProvider, tradeParser, tradeStorage);
             tradeProcessor.ProcessTrades();
+
+           
+
+
+
+
+
 
             Console.ReadKey();
         }
---
+++ SingleResponsibilityPrinciple/URLTradeDataProvider.cs
@@ -0,0 +1,45 @@
+﻿using SingleResponsibilityPrinciple.Contracts;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+// ===============================
+// AUTHOR      : Sumathilatha Myla
+// CREATE DATE :11/03/2019
+// PURPOSE     : Read the trades from a remote call to a web service
+// ===============================
+
+namespace SingleResponsibilityPrinciple
+{
+    public class URLTradeDataProvider : ITradeDataProvider
+    {
+        private readonly string  url;
+        public URLTradeDataProvider(string url)
+        {
+            this.url = url;
+        }
+        /// <summary>
+        /// This method Reads data from webservice
+        /// </summary>
+        /// <returns>Trade data </returns>
+        public IEnumerable<string> GetTradeData()
+        {
+            var tradeData = new List<string>();
+            var client = new WebClient();
+            using (var urlData = client.OpenRead(url))
+            using (var reader = new StreamReader(urlData))
+            { 
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    tradeData.Add(line);
+                }
+            }
+            return tradeData;
+
+        }
+    }
+}
